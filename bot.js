f11e474e74a7a93a66a3fe017daa94e606ad158b
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-04-25 19:09:27 -0700

    Response Changes

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
 
   client.on("messageCreate", (message) => {
     if (message.content.toUpperCase() === `TPC`) {
-      message.reply("The Pilot Club Welcomes You!")
+      message.reply("Welcome to The Pilot Club!")
     }
   })
 
