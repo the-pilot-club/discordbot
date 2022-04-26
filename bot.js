53804ab713847eb42f4872422581dd48fc365280
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-04-25 20:52:24 -0700

    fixed bot.

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('ready', () => {
 
   client.on("messageCreate", (message) => {
     if (message.content.toLowerCase() === "support") {
-    message.reply({ content: "To get support or submit feedback, click here: https://support.thepilotclub.org/open.php  Thank you for being a vauled member of The Pilot Club!!")
+    message.reply("To get support or submit feedback, click here: https://support.thepilotclub.org/open.php  Thank you for being a vauled member of The Pilot Club!!")
         }
   })
 
