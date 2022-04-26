6c0d7fe5bc36891dbfb22cd141b07b6e8a9ad7b7
thepilotclub <webdev@thepilotclub.net>
2022-04-25 20:23:02 -0500

    inital setup

---
+++ bot.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const { Client, Collection, Intents } = require('discord.js');
+const client = new Client({ intents: 32767 });
+const prefix = "$"
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+
+  client.on("messageCreate", (message) => {
+    if (message.content.toUpperCase() === `TPC`) {
+      message.reply("Welcome to The Pilot Club")
+    }
+  })
+
+  client.login(process.env.BOT_TOKEN)
