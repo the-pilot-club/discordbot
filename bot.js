afe58e87e340165938ebdca14fce902d530b2342
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-04-25 20:33:58 -0700

    changed message on eric response

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('ready', () => {
 
   client.on("messageCreate", (message) => {
     if (message.content.toLowerCase() === "eric") {
-      message.reply("<@398557782623649794> That one guy who knew enough coding to make me a thing. If you are seeing this message, DM him and tell him the code word || Green Horn ||")
+      message.reply("<@398557782623649794> That one guy who knew enough coding to make me a thing (with help from <@875527822611992577>). If you are seeing this message, DM him and tell him the code word || Green Horn ||")
     }
   })
 
