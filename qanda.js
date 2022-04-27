cab3787c8aa51209d504fcc83b32ba7606dce072
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-04-26 19:29:53 -0700

    trying to get qanda working

--- qanda.js
+++ qanda.js
@@ -30,3 +30,10 @@ cron.schedule('0 0 * * *', function() {
        //TODO: DELETE ITEM FROM ARRAY
    }, 1000)//Wait 12 hours: 1000 * 60 * 60 * 12
 });
+
+
+client.on("messageCreate", (message) => {
+  if (message.content.toLowerCase() === "eric") {
+    message.reply("<@398557782623649794> That one guy who knew enough coding to make me a thing (with help from <@875527822611992577>). If you are seeing this message, DM him and tell him the code word || Green Horn ||")
+  }
+})
