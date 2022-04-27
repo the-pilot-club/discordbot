b5223a30f36f8968facc18f317b2c165da00d81c
thepilotclub <webdev@thepilotclub.net>
2022-04-26 20:45:40 -0500

    fix?

--- bot.js
+++ bot.js
@@ -5,12 +5,12 @@ const client = new Client({ intents: 32767 });
 const prefix = "$"
 const { MessageActionRow, MessageButton } = require('discord.js');
 
-/*
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity('XPlane 11' , {type: "PLAYING"})
   });
-
+/*
   client.on("messageCreate", (message) => {
     if (message.content.toUpperCase() === `TPC WELCOME`) {
       message.reply("Welcome to The Pilot Club!")
