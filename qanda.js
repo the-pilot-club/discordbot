64b166405f097cc26db701564a61d95538af3902
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-04-26 19:47:06 -0700

    trying to get qanda working

--- qanda.js
+++ qanda.js
@@ -18,7 +18,8 @@ let questions = file.questions;
 //Getting random question
 console.log(questions[randomNum(0,14)][0])
 
- client.channels.cache.get('864834861603487754').send('Test!')
+ client.channels.cache.get('864834861603487754');
+ channel.send('Test!')
 //Getting random question every day at 8am
 cron.schedule('0 0 * * *', function() {
   let generatedNum = randomNum(0,14)
@@ -30,10 +31,3 @@ cron.schedule('0 0 * * *', function() {
        //TODO: DELETE ITEM FROM ARRAY
    }, 1000)//Wait 12 hours: 1000 * 60 * 60 * 12
 });
-
-
-client.on("messageCreate", (message) => {
-  if (message.content.toLowerCase() === "eric") {
-    message.reply("<@398557782623649794> That one guy who knew enough coding to make me a thing (with help from <@875527822611992577>). If you are seeing this message, DM him and tell him the code word || Green Horn ||")
-  }
-})
