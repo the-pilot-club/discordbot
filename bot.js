61fd4e911e5774fa56b9257568015827359f57d0
Lucas Immerman <lucasimmerman@gmail.com>
2022-04-27 20:09:46 -0400

    yet another bug fix

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ function randomNum(min, max) {
 //Parsing questions
 let questions = file.questions;
 //Getting random question
-console.log(questions[randomNum(0,questions.length())][0])
+console.log(questions[randomNum(0,questions.length)][0])
 
 //sends message to a specific channel
 client.on('ready', async function() {
@@ -73,7 +73,7 @@ client.on('ready', async function() {
   channel.send('The Bot has Restarted or has been 24 hours. Who knows? I dont.');
 //Getting random question every day at 8am
 cron.schedule('*/1 * * * *', function() {
-  let generatedNum = randomNum(0,questions.length())
+  let generatedNum = randomNum(0,questions.length)
   console.log(questions[generatedNum])//[0])
     channel.send(questions[generatedNum][0])
    setTimeout(() => {
