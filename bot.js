5f2ffb8846ee27626b7d267968767355972862ac
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-04-27 18:13:11 -0700

    fixed typo

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ client.on('ready', () => {
 
   client.on("messageCreate", (message) => {
     if (message.content.toLowerCase() === "support") {
-    message.reply("To get support or submit feedback, click here: https://support.thepilotclub.org/open.php  Thank you for being a vauled member of The Pilot Club!!")
+    message.reply("To get support or submit feedback, click here: https://support.thepilotclub.org/open.php  Thank you for being a valued member of The Pilot Club!!")
         }
   })
 
