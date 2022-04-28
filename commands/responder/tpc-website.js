c853ff6fc95a2bd7e1941c21530bf85278ec9c74
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-04-27 18:48:09 -0700

    folder for responder?

--- commands/responder/tpc-website.js
+++ commands/responder/tpc-website.js
@@ -1,9 +1,3 @@
-const { Client, Collection, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const prefix = "$"
-const { MessageActionRow, MessageButton } = require('discord.js');
-const fs = require('fs');
-
 client.on("messageCreate", (message) => {
     if (message.content.toLowerCase() === `tpc website`) {
       const row = new MessageActionRow()
