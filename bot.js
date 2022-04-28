c53e2f0c185e128a874554eb220039b37f53a062
Lucas Immerman <lucasimmerman@gmail.com>
2022-04-27 20:17:42 -0400

    intents

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const { Client, Collection, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const prefix = "$"
 const { MessageActionRow, MessageButton } = require('discord.js');
 const fs = require('fs');
