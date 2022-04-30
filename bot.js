99286a895e74196376c80547bfb596fec2d62c75
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-04-29 17:28:38 -0700

    fixed q and a (again)

--- bot.js
+++ bot.js
@@ -89,10 +89,10 @@ let question=questions[index];
 client.on('ready', async function() {
   const channel = await client.channels.fetch(process.env.CHANNEL_ID);
 //Getting random question every day:  0 57 22 * * * 
-  cron.schedule('*/0.5 * * * *', function() {
+  cron.schedule('0 55 19 * * *', function() {
     sendNewQuestion(channel);
   });
-  cron.schedule('*/2 * * * *', function() {
+  cron.schedule('0 0 20 * * *', function() {
     sendNewAnswer(channel);
   });
 });
