dab8d9d7f7d9cab92e72faba903aa22dbced04a6
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-05-06 22:13:32 -0700

    removed responder

--- events/club-livery.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'messageCreate',
-    once: false,
-      execute(message) {
-      if (message.content.toLowerCase() === "club livery")
-      message.reply("Club liveries can be downloaded here: https://www.thepilotclub.org/sop#liveries")
-      }
-  };
