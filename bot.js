9d35e9d06f012b0eae370e9e4a5dcbf989489ee5
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-05-07 13:12:51 -0700

    /commands update

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ for (const file of eventFiles) {
         .addComponents(
           new MessageButton()
           .setLabel('Next TPC Group Flight')
-          .setURL("https://www.thepilotclub.org/dispatch")
+          .setURL("https://thepilotclub.org/dispatch")
           .setStyle('LINK'),
         ) ;
       await interaction.reply({content:`Next TPC Group Flight:`, components: [row]})
