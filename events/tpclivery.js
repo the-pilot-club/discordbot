1b667f1de9cd507e1b96d89fe6639d7644347924
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-05-07 13:21:22 -0700

    event update

--- events/tpclivery.js
+++ events/tpclivery.js
@@ -3,6 +3,6 @@ module.exports = {
     once: false,
       execute(message) {
       if (message.content.toLowerCase() === "tpc livery")
-      message.reply("Club liveries can be downloaded here: https://www.thepilotclub.org/sop#liveries")
+      message.reply("Club liveries can be downloaded here: https://thepilotclub.org/sop#liveries")
       }
   };
