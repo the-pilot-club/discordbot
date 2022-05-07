546253c8b1f5f16eca2aa6b367e4e96b0178e0d5
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-05-06 22:14:15 -0700

    updated responder

--- events/fno.js
+++ events/fno.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'messageCreate',
     once: false,
       execute(message) {
-      if (message.content.toLowerCase() === "what is fno")
+      if (message.content.toLowerCase() === "what is fno?")
       message.reply("FNO Stands for Friday Night Ops. You can find more information here: https://docs.google.com/document/d/1n2dorXXbRavCci0FqYMMDQngrYqnn3UXNDAiK95Kc98/edit")
       }
   };
