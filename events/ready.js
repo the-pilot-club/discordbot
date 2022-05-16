dff9ee3abc90e1a2091ae6b7ca95acc8b9872bdc
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-05-15 22:03:22 -0700

    erics humor

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Logged in as ${client.user.tag}`);
-        client.user.setActivity('To Seattle Center on 135.450' , {type: "LISTENING"})
+        client.user.setActivity('Seattle Center on 135.450' , {type: "LISTENING"})
 	},
 };
