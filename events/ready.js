f56b5831b8f8e80f27fedb35e03da57b0deea81f
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-05-22 15:01:03 -0700

    status message

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Logged in as ${client.user.tag}`);
-        client.user.setActivity('Seattle Center on 135.450' , {type: "LISTENING"})
+        client.user.setActivity('XPlane 11' , {type: "PLAYING"})
 	},
 };
