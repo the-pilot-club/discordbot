b6a03cbbe8bfec055a512a54cee5a60947c3738f
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-05-27 14:46:49 -0700

    change ID for charters

--- bot.js
+++ bot.js
@@ -115,8 +115,8 @@ client.on('guildMemberUpdate', async (oldMember, newMember) => {
     })
  //dm for charters role
  client.on('guildMemberUpdate', async (oldMember, newMember) => {
-    if(oldMember.roles.cache.has('979566038452666375')) return;
-    if(newMember.roles.cache.has('979566038452666375')) {
+    if(oldMember.roles.cache.has('897118707988451339')) return;
+    if(newMember.roles.cache.has('897118707988451339')) {
     newMember.user.send( "Welcome to TPC Charters, we look forward to having you fly with us. Please read https://www.thepilotclub.org/s/TPC_Charters_-_pilots_guide_v10.pdf.  We hope this will answer most of your questions.  If you still have questions after having read this, then ask away."
       )
  }
