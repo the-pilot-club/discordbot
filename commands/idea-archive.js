1731b78e82ca5510ba95886a12fcf69f8cf4ce60
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-05-26 20:36:44 -0700

    added reason given for archive

--- commands/idea-archive.js
+++ commands/idea-archive.js
@@ -11,22 +11,39 @@ module.exports = {
             option.setName('suggestion-number').setDescription('What is the Idea Number?').setRequired(true)
         ).addStringOption(option=>
             option.setName('idea-details').setDescription('Copy and paste the idea details').setRequired(true)
+        ).addStringOption(option=>
+            option.setName('reason-given').setDescription('Copy and paste the reason given details').setRequired(false)
         ),
 	async execute(interaction,client) {
-        const channel = interaction.guild.client.channels.cache.get('979578236122071050')
+        const channel = interaction.guild.client.channels.cache.get('979566041061552217')
         const number = interaction.options.getString('suggestion-number')
         const idea = interaction.options.getString('idea-details')
-        const embed = new MessageEmbed()
+        const reason = interaction.options.getString('reason-given')
+        const noreasonembed = new MessageEmbed()
             .setAuthor({name:`${interaction.user.tag}`, iconURL:`${interaction.user.displayAvatarURL()}`})
             .setTitle(`Implemented Idea Archive for Suggestion #${number}`)
             .setColor('0X37B6FF')
-            .addFields({name: `Suggestion Deatails:`, value:`${idea}`})
+            .addFields({name: `Suggestion Deatails:`, value:`${idea}`}) 
             .setFooter({text: 'Made by The Pilot Club For Adminisration'});
-        if (interaction.member.roles.cache.some(role => role.name === 'Air Marshals')){
-            channel.send({embeds: [embed]})
-            await interaction.reply({content:`The idea has been posted in the archive channel`, ephemeral: true})
+
+        const reasonembed = new MessageEmbed()
+        .setAuthor({name:`${interaction.user.tag}`, iconURL:`${interaction.user.displayAvatarURL()}`})
+        .setTitle(`Implemented Idea Archive for Suggestion #${number}`)
+        .setColor('0X37B6FF')
+        .addFields({name: `Suggestion Deatails:`, value:`${idea}`})
+        .addFields({name: `Reason Given`, value: `${reason}`})  
+        .setFooter({text: 'Made by The Pilot Club For Adminisration'});
+
+    if (interaction.member.roles.cache.some(role => role.name === 'Air Marshals')){
+        if (reason !== null){     
+            channel.send({embeds: [reasonembed]})
         } else {
-            interaction.reply({content:`This command is only for the Admin of The Pilot Club. Please try another command.`, ephemeral: true})
+            channel.send({embeds: [noreasonembed]})
         }
+        await interaction.reply({content:`The idea has been posted in the archive channel`, ephemeral: true})
+    } else {
+        interaction.reply({content:`This command is only for the Admin of The Pilot Club. Please try another command.`, ephemeral: true})
     }
+}
+
 }
