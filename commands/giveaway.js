af4291fae08e11ccf8d1d37a4f1cc58ea8a10d4e
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-05-30 18:46:57 -0700

    funtion giveaway

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -10,8 +10,7 @@ module.exports = {
             interaction.guild.members.fetch().then(members => {
                 let result = members.filter(m => m.roles.cache.find(role => role.id == '860938566426558505'));
                 let tags = result.map(m=>m.user.tag);
-                //channel.send("The winner is: " + tags[Math.round(Math.random() * tags.length)] + " Congrats!");
-                console.log(result.size, tags.length);
+                channel.send("The winner is: " + tags[Math.round(Math.random() * tags.length)] + " Congrats!");
             })
         }
 	},
