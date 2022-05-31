8b4756817abb74143264d689143f5e88b4532776
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-05-30 18:06:28 -0700

    bug fix

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -2,12 +2,12 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data:  new SlashCommandBuilder()
-        .setName('giveaway')
-        .setDescription('picks random user with giveaway role'),
+            .setName('giveaway')
+            .setDescription('picks random user with giveaway role'),
 	async execute(interaction) {
-    if (interaction.member.roles.cache.some(role => role.name === 'Staff') || interaction.member.roles.cache.some(role => role.name === 'Air Marshals')){
-    let members = interaction.guild.roles.cache.get('860938566426558505').members.map(m=>m.user.tag)
-    interaction.reply("The winner is: " + members[Math.floor(Math.random() * members.length)] + " Congrats!");
-    }
+        if (interaction.member.roles.cache.some(role => role.name === 'Staff') || interaction.member.roles.cache.some(role => role.name === 'Air Marshals')){
+            let members = interaction.guild.roles.fetch('860938566426558505').members.map(m=>m.user.tag)
+            interaction.reply("The winner is: " + members[Math.round(Math.random() * members.length)] + " Congrats!");
+        }
 	},
 };
