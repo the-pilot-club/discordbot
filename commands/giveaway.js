426316c8ee8f8349e892664854db184e35300202
Losh531 <lucasimmerman@gmail.com>
2022-05-30 20:53:33 -0400

    fix roles

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -5,7 +5,9 @@ module.exports = {
         .setName('giveaway')
         .setDescription('picks random user with giveaway role'),
 	async execute(interaction) {
+    if (interaction.member.roles.cache.some(role => role.name === 'Staff') || interaction.member.roles.cache.some(role => role.name === 'Air Marshals')){
     var members = interaction.guild.roles.cache.get('860938566426558505').members.map(m=>m.user.tag)
     interaction.reply("The winner is: " + members[Math.floor(Math.random() * members.length)] + " Congrats!");
+    }
 	},
 };
