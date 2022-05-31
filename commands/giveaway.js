613d0187d32b69da469f167c1e143939f56940c4
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-05-30 18:49:49 -0700

    interaction test

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -6,11 +6,11 @@ module.exports = {
             .setDescription('picks random user with giveaway role'),
 	async execute(interaction) {
         if (interaction.member.roles.cache.some(role => role.name === 'Staff') || interaction.member.roles.cache.some(role => role.name === 'Air Marshals')){
-            let channel = interaction.channel;
+            //let channel = interaction.channel;
             interaction.guild.members.fetch().then(members => {
                 let result = members.filter(m => m.roles.cache.find(role => role.id == '860938566426558505'));
                 let tags = result.map(m=>m.user.tag);
-                channel.send("The winner is: " + tags[Math.round(Math.random() * tags.length)] + " Congrats!");
+                interaction.reply("The winner is: " + tags[Math.round(Math.random() * tags.length)] + " Congrats!");
             })
         }
 	},
