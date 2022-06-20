967b4db1b3baf79471562512bccdeb86d4724ddb
Losh531 <lucasimmerman@gmail.com>
2022-06-19 22:05:53 -0400

    ping instead of name

--- events/leaderboard.js
+++ events/leaderboard.js
@@ -9,7 +9,12 @@ module.exports = {
     Mee6LevelsApi.getLeaderboardPage(guildId).then(leaderboard => {
       //get the top 5 users
       var top5 = leaderboard.slice(0,5)
-      message.reply(`Our **Top 5** of the week: \n${top5.map(user => user.username).join('\n')}\nSee all rankings here: https://vats.im/tpc-leaderboard`)
+      let list = top5.map(user => user.id)
+      var formatted = ""
+      for (var i = 0; i < list.length; i++){
+        formatted+= "\n" +("<@" + list[i] + ">")
+      }
+      message.reply(`Our **Top 5** of the week: \n${formatted}\nSee all rankings here: https://vats.im/tpc-leaderboard`)
     }).catch(err => {
     });
   }
