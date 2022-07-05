975004cf52d9b80635250710ae9dda207339eca0
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-07-04 17:38:59 -0700

    connect command for testing

---
+++ commands/connect.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('connect')
+		.setDescription('Connect your Discord Account to your VATSIM Account in the pilot Club!'),
+	async execute(interaction) {
+        const row = new MessageActionRow()
+        .addComponents(
+          new MessageButton()
+          .setLabel('Connect my account!')
+          .setURL(`https://callsigns.thepilotclub.org/sendauthentication?id=${interaction.user.id}`)
+          .setStyle('LINK'),
+        ) ;
+      await interaction.reply({content:`Please use this button to connect your account!`, components: [row]})
+}
+};
