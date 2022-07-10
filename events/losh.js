fb241609a069ebbb2efc6863d791f1052eef0dbb
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-07-09 22:02:17 -0700

    code move around to make easier to read

--- events/losh.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'messageCreate',
-    once: false,
-      execute(message) {
-      if (message.content.toLowerCase() === "losh")
-      message.reply("is better than you...Just Kidding! <3 you Losh (Eric)")
-      }
-  };
