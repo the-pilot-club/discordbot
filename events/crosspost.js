55b011931638aa7ff4a67470b383662993f6b89e
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-08-01 16:40:41 -0700

    remove crosspost

--- events/crosspost.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'messageCreate',
-    once: false,
-    execute(message) {
-        if (message.channel.type === 'GUILD_NEWS') {
-            message.crosspost()
-        }
-    }
-}
