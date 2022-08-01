97f38383f6761d39004f901ce84270fcd69045bb
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-08-01 16:48:39 -0700

    added delay to giveaway command

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -9,7 +9,11 @@ module.exports = {
             interaction.guild.members.fetch().then(members => {
                 let result = members.filter(m => m.roles.cache.find(role => role.id == process.env.GIVEAWAY_ROLE));
                 let tags = result.map(m => m.user.tag);
-                interaction.reply("And the winner is " + tags[Math.floor(Math.random() * tags.length)] + ". Congratulations!");
+                 interaction.deferReply()
+                 setTimeout(function (){
+                     interaction.editReply("And the winner is " + tags[Math.floor(Math.random() * tags.length)] + ". Congratulations!");
+                 },3000)
+
             })
         } else {
             interaction.reply("Sorry, /giveaway is staff only")
