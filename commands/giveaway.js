afff8bdb93dba353adeed1a7596b40ab769ca38c
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-08-01 17:15:13 -0700

    tag user in giveaway

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -8,7 +8,7 @@ module.exports = {
         if (interaction.member.roles.cache.some(role => role.name === 'Staff') || interaction.member.roles.cache.some(role => role.name === 'Air Marshals')) {
             interaction.guild.members.fetch().then(members => {
                 let result = members.filter(m => m.roles.cache.find(role => role.id == process.env.GIVEAWAY_ROLE));
-                let tags = result.map(m => m.user.tag);
+                let tags = result.map(m => m.user.toString());
                  interaction.deferReply()
                  setTimeout(function (){
                      interaction.editReply("And the winner is " + tags[Math.floor(Math.random() * tags.length)] + ". Congratulations!");
