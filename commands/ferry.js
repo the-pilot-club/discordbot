c89cf0078ba95723af7d17e4a872e85f483920e2
vFlyingCowboy <wethepeoplesteiner@gmail.com>
2022-11-19 21:51:26 -0800

    gneral updates

--- commands/ferry.js
+++ commands/ferry.js
@@ -1,42 +1,33 @@
-const {SlashCommandBuilder} = require('discord.js');
+const {SlashCommandBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder} = require('discord.js');
 const {EmbedBuilder} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('charters-ferry-request')
         .setDescription('Use this command to request an aircraft to be ferried to another location.'
-        ).addStringOption(option =>
-            option.setName('aircraft-registration').setDescription('What is the Tail Number for the aircraft you would like ferried?').setRequired(true)
-        ).addStringOption(option =>
-            option.setName('starting-icao').setDescription('Where is the Aircraft located?').setRequired(true)
-        ).addStringOption(option =>
-            option.setName('ending-icao').setDescription('Where would you like the aircraft to go?').setRequired(true)
         ),
     async execute(interaction) {
-        const channel = interaction.guild.client.channels.cache.get(process.env.CHARTERS_REQUEST_CHANNEL)
-        const end = interaction.options.getString('ending-icao')
-        const start = interaction.options.getString('starting-icao')
-        const tail = interaction.options.getString('aircraft-registration')
-        const embed = new EmbedBuilder()
-            .setAuthor({name: `${interaction.member.displayName}`, iconURL: `${interaction.user.displayAvatarURL()}`})
-            .setTitle('New Ferry Request')
-            .setColor('0X37B6FF')
-            .addFields({
-                name: '\u200b',
-                value: `**TPC Charters User:** ${interaction.user} \n**Registration Number:** ${tail.toUpperCase()} \n**Starting Location:** ${start.toUpperCase()} \n**Ending Location** ${end.toUpperCase()}`
-            })
-            .setFooter({text: 'Made by The Pilot Club For TPC Charters'});
-        if (interaction.member.roles.cache.some(role => role.name === 'Charters Pilots')) {
-            channel.send({content: `<@&910012872246046730>`, embeds: [embed]})
-            await interaction.reply({
-                content: `You have requested for tail number ${tail.toUpperCase()} to be ferried from ${start.toUpperCase()} to ${end.toUpperCase()}. A Charters Manager will ferry the aircraft as soon as they can. If you do not have the aircraft moved within 12 hours, please try this command again.`,
-                ephemeral: true
-            })
-        } else {
-            interaction.reply({
-                content: `You do not have the Charters Pilots Role! Go to #about-and-sop to learn more!`,
-                ephemeral: true
-            })
-        }
+        const modal  = new ModalBuilder()
+            .setCustomId('charter-ferry')
+            .setTitle('Ferry a TPC Charters Aircraft')
+        const tail = new TextInputBuilder()
+            .setCustomId('aircraft-registration')
+            .setLabel('What is the Tail Number?')
+            .setStyle(TextInputStyle.Short)
+        const start = new TextInputBuilder()
+            .setCustomId('starting-icao')
+            .setLabel('What is your starting location?')
+            .setStyle(TextInputStyle.Short)
+        const end = new TextInputBuilder()
+            .setCustomId('ending-icao')
+            .setLabel('What is your ending location?')
+            .setStyle(TextInputStyle.Short)
+        const actionrow1 = new ActionRowBuilder().addComponents(tail)
+        const actionrow2 = new ActionRowBuilder().addComponents(start)
+        const actionrow3 = new ActionRowBuilder().addComponents(end)
+
+        modal.addComponents(actionrow1,actionrow2,actionrow3)
+
+        await interaction.showModal(modal)
     }
 }
