f07bb4e110805b7173942557beee62fcac9df1d7
jonahkleinman <97543307+jonahkleinman@users.noreply.github.com>
2023-06-07 11:24:06 -0400

    fallback if airport doesnt exist

--- commands/airport.js
+++ commands/airport.js
@@ -14,7 +14,10 @@ module.exports = {
         const airport = interaction.options.getString('icao')
         const response = await fetch(`https://metar.vatsim.net/metar.php?id=${airport}`);
         const body = await response.text();
-
+  if (!body) {
+            interaction.reply(`${airport} does not exist!`);
+            return;
+        }
         let airportEmbed = new EmbedBuilder()
             .setTitle('Airport')
             .setDescription(`Information about ${airport.toUpperCase()}`)
