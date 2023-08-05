7c0b4e27fc3170ac67d7944df480419692b764dd
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2023-08-04 20:41:28 -0700

    make work correctly lol

--- commands/next-flight.js
+++ commands/next-flight.js
@@ -10,29 +10,36 @@ module.exports = {
         const events = await interaction.guild.scheduledEvents.fetch();
         const sortedEvents = events.sort((a, b) => a.scheduledStartAt - b.scheduledStartAt);
         const event = sortedEvents.first();
-        const embed = new EmbedBuilder()
-            .setAuthor({name: `${event.name}`})
-            .setDescription(`${event.description}` ?? 'Not included')
-            .addFields({name: 'Event Start Time:', value: `${event.scheduledStartAt}`}, {
-                name: 'Voice Channel:',
-                value: event.channelId ?  `<#${event.channelId}>` :'Not Included'
-            })
-            .setColor('#37B6FF')
-            .setImage(event.coverImageURL())
-            .setTimestamp()
 
-            .setFooter({
-                text: "Made by TPC Dev Team",
-                iconURL: `https://static1.squarespace.com/static/614689d3918044012d2ac1b4/t/616ff36761fabc72642806e3/1634726781251/TPC_FullColor_TransparentBg_1280x1024_72dpi.png`
-            })
-            .setTimestamp()
-        const row = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setLabel("More TPC Group Flights")
-                    .setURL("https://thepilotclub.org/dispatch")
-                    .setStyle(ButtonStyle.Link),
-            );
-        await interaction.reply({content: `Next TPC Group Flight:`, embeds: [embed], components: [row]})
+        if (event === undefined) {
+            interaction.reply('No events :(')
+        } else {
+
+
+            const embed = new EmbedBuilder()
+                .setAuthor({name: `${event.name ? event.name: 'Not included'}`})
+                .setDescription(`${event.description ? event.description : 'Not Included'}`)
+                .addFields({name: 'Event Start Time:', value: `${event.scheduledStartAt}`}, {
+                    name: 'Voice Channel:',
+                    value: event.channelId ? `<#${event.channelId}>` : 'Not Included'
+                })
+                .setColor('#37B6FF')
+                .setImage(event.coverImageURL({size: 4096}))
+                .setTimestamp()
+
+                .setFooter({
+                    text: "Made by TPC Dev Team",
+                    iconURL: `https://static1.squarespace.com/static/614689d3918044012d2ac1b4/t/616ff36761fabc72642806e3/1634726781251/TPC_FullColor_TransparentBg_1280x1024_72dpi.png`
+                })
+                .setTimestamp()
+            const row = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setLabel("More TPC Group Flights")
+                        .setURL("https://thepilotclub.org/dispatch")
+                        .setStyle(ButtonStyle.Link),
+                );
+            await interaction.reply({content: `Next TPC Group Flight:`, embeds: [embed], components: [row]})
+        }
     }
 };
