05f209b6f46fff65a260e8a3b54205353e0e11b9
jonahkleinman <97543307+jonahkleinman@users.noreply.github.com>
2023-10-03 21:01:07 -0400

    add reactions to screenshots channel

---
+++ events/tpcreaction.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'messageCreate',
+    once: false,
+    execute (message) {
+        if (message.channel.id === '830210202464813056') {
+            message.react(':tpc:845075689241051138');
+          }
+      }
+    }
+  
+  
