423cc2a47ee7f5218642ddc32ce67bab4ee5b600
jonahkleinman <97543307+jonahkleinman@users.noreply.github.com>
2023-10-05 19:31:19 -0400

    thread support

--- events/tpcreaction.js
+++ events/tpcreaction.js
@@ -2,10 +2,11 @@ module.exports = {
     name: 'messageCreate',
     once: false,
     execute (message) {
-        if (message.channel.id === '830210202464813056') {
-            message.react(':tpc:845075689241051138');
-          }
+  if (message.thread?.name.startsWith('SCREENSHOT CONTEST') || message.channel.id === '830210202464813056') {
+    const image = message.attachments.find(attachment => attachment.contentType?.startsWith('image/'));
+    if (image) {
+      await message.react(':tpc:845075689241051138');
+    }
       }
     }
-  
-  
+  }
