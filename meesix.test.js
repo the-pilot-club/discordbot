60edb9d3a36bf68a84da0cc26e19a1127bbfd67a
Losh533 <lucasimmerman2@gmail.com>
2023-10-06 21:28:30 -0400

    mee6 tests

--- meesix.test.js
+++ meesix.test.js
@@ -1,16 +1,18 @@
-const Mee6LevelsApi = require('mee6-levels-api')
-const mee6 = require('./meesix.js');
-const guildId = '830201397974663229'
+/* eslint-env jest */
+/* eslint-disable jest/no-conditional-expect */
 
+const Mee6LevelsApi = require('mee6-levels-api')
+const mee6 = require('./meesix.js')
+const guildId = '830201397974663229'
 
 test('checks if mee6 API is working', () => {
   return Mee6LevelsApi.getLeaderboardPage(guildId)
     .then(leaderboard => {
       const list = leaderboard
-      expect(mee6(list)).toBe(true);
+      expect(mee6(list)).toBe(true)
     })
     .catch(err => {
       // You might want to fail the test if the promise is rejected
-      throw err;
-    });
-});
+      throw err
+    })
+})
