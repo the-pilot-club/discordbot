9ae84c237d6a9b3fe1e07d06cc2abff0ff7e18d3
jonahkleinman <97543307+jonahkleinman@users.noreply.github.com>
2023-10-08 20:36:55 -0400

    fix pr changes

--- events/tpcreaction.js
+++ events/tpcreaction.js
@@ -1,11 +1,11 @@
 module.exports = {
     name: 'messageCreate',
     once: false,
-    execute (message) {
+    async execute (message) {
   if (message.thread?.name.startsWith('SCREENSHOT CONTEST') || message.channel.id === '830210202464813056') {
     const image = message.attachments.find(attachment => attachment.contentType?.startsWith('image/'));
     if (image) {
-      await message.react(':tpc:845075689241051138');
+      message.react(':tpc:845075689241051138');
     }
       }
     }
