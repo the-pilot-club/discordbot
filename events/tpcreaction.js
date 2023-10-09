9daa04f796a1c599f1d0605f9f216b8c7fa47f10
jonahkleinman <97543307+jonahkleinman@users.noreply.github.com>
2023-10-08 20:41:11 -0400

    change 2

--- events/tpcreaction.js
+++ events/tpcreaction.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'messageCreate',
     once: false,
-    async execute (message) {
+    execute (message) {
   if (message.thread?.name.startsWith('SCREENSHOT CONTEST') || message.channel.id === '830210202464813056') {
     const image = message.attachments.find(attachment => attachment.contentType?.startsWith('image/'));
     if (image) {
