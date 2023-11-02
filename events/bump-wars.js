7c2d95becac816f8d28cd8222b2027ec7ff7df4b
michaelcmelton <michaelcmelton93@gmail.com>
2023-11-01 21:14:31 -0400

    feat(bump wars): Update copy

--- events/bump-wars.js
+++ events/bump-wars.js
@@ -4,17 +4,18 @@ module.exports = {
   execute (message) {
     if (message.content.toLowerCase() === 'bump wars') {
       message.reply('**[BUMP WARS](https://discord.com/channels/830201397974663229/958549204073087086)** \n \n' +
-                '__Team 1: Law and Order__\n\n- Dylan | TPC1496 | DELA\n- Serge | TPC6\n- Kelvin | TPC1992 | SBxx' +
-                '\n\n__Team 2: Angry Monkeys__\n\n- Caleb Y | TPC452 | 787\n- Rich P | N7RP\n- Chris | TPC139 | KUHC' +
+        '__Team 1: Hot Dogs__\n\n- <@130510720763822080>\n- <@715881255027081238>\n- <@796121185825062963>' +
+        '\n\n__Team 2: Weiner Dogs__\n\n- <@524567291128709140>\n- <@628118960637870090>\n- <@601567349669363722>' +
                 ' \n \n__Rules:__ \n' +
                 '1: type `/bump` to bump the server on Disboard \n' +
                 '2: Bumps are only possible once every 2 hours \n' +
                 '3: If your bump is successful you must post the current score under your bump or forfeit the point (nobody gets a point) \n' +
                 '4: Have fun! \n' +
+        '5: This war starts on 11/01 0400z (00:00 ET) and ends on 12/01 0359z (11/30 23:59 ET)\n' +
                 '\nImportant info: \n' +
-                "*Daily score resets at midnight Eastern time. Any bumps made past that moment will be counted for the next day's score." +
-                '\n*Teams compete to win the week by majority of days won. \n*The team with the most weeks under their belt at the end of the month wins! \n' +
-                '\nWhy are we doing this? \nBumping this server often helps to keep us at the top of the server list on Disboard.' +
+        "* If there are more than one bump at a time, only those claimed will be valid, no matter how many there are.\n" +
+        "* The team with the most bumps under their belt at the end of the month wins!\n" +
+        "* Winning team members get 1000 TPC points and a shout-out during next town-hall" +
                 ' It gives our community a chance to grow and allows you to be involved in the process. Have fun!')
     }
   }
