5de934c422c27d048291d36ecc566facdba2a8ed
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2023-11-07 19:14:09 -0800

    update main branch (#233)
    Merge command fixes and deployment file updates
    ---------
    
    Co-authored-by: jonahkleinman <97543307+jonahkleinman@users.noreply.github.com>
    Co-authored-by: Mike Melton <michaelcmelton93@gmail.com>

--- commands/hours.js
+++ commands/hours.js
@@ -1,63 +1,82 @@
-const { SlashCommandBuilder } = require('discord.js')
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
-const { EmbedBuilder } = require('discord.js')
-const fetch = require('node-fetch')
+const { SlashCommandBuilder } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+const fetch = require('node-fetch');
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('hours')
     .setDescription('See how many hours you have on the network!'),
-  async execute (interaction) {
-    const response = await fetch(`https://callsigns.thepilotclub.org/DiscordOperations/GetVatsimHrsInfo?Discordid=${interaction.user.id}`, {
-      method: 'POST'
-    })
-    const body = await response.json()
-    if (body === 'Not Found') {
+  async execute(interaction) {
+    const response = await fetch(`https://api.vatsim.net/v2/members/discord/${interaction.user.id}`, {
+      method: 'GET',
+    });
+
+    if (!response || response.status !== 200) {
+      console.log(`The Response could not be completed as dialed for ${interaction.member.displayName}`);
+      interaction.reply({ content: 'This Function could not be completed as dialed. Please try again later', ephemeral: true });
+      return;
+    }
+
+    const body = await response.json();
+    const notFound = body.detail;
+    const cid = body.user_id;
+
+    if (notFound === 'Not Found') {
       const row = new ActionRowBuilder()
         .addComponents(
           new ButtonBuilder()
             .setLabel('Connect my account!')
-            .setURL(`https://callsigns.thepilotclub.org/sendauthentication.aspx?id=${interaction.user.id}`)
+            .setURL('https://community.vatsim.net')
             .setStyle(ButtonStyle.Link)
-        )
-      await interaction.reply({ content: 'Please connect your VATSIM account to the TPC Discord!', components: [row], ephemeral: true })
+        );
+      await interaction.reply({
+        content: 'Please connect your VATSIM account to the VATSIM Community Hub!',
+        components: [row],
+        ephemeral: true,
+      }).catch(err => {
+        console.log(err);
+      });
     } else {
-      const data = JSON.parse(body)
+      const hoursResponse = await fetch(`https://api.vatsim.net/v2/members/${cid}/stats`);
+
+      if (!hoursResponse || hoursResponse.status !== 200) {
+        console.log(`The hoursResponse could not be completed as dialed for ${interaction.member.displayName}`);
+        interaction.reply({ content: 'The stats function could not be completed as dialed. Please try again later', ephemeral: true });
+        return;
+      }
+
+      const data = await hoursResponse.json();
+      const hoursData = JSON.parse(data)
+
       const embed = new EmbedBuilder()
-        .setAuthor({ name: `${interaction.member.displayName} - ${data.id}`, iconURL: `${interaction.user.displayAvatarURL()}` })
+        .setAuthor({ name: `${interaction.member.displayName} - ${cid}`, iconURL: `${interaction.user.displayAvatarURL()}` })
         .setTitle('Your Hours On VATSIM!')
-        .addFields({ name: 'Pilot Hours:', value: `${data.pilot}`, inline: true },
-          { name: 'ATC Hours:', value: `${data.atc}` })
+        .addFields({ name: 'Pilot Hours:', value: `${hoursData.pilot}`, inline: true },
+          { name: 'ATC Hours:', value: `${hoursData.atc}` })
         .setColor('#37B6FF')
         .setFooter({ text: 'Made by TPC Dev Team', iconURL: 'https://static1.squarespace.com/static/614689d3918044012d2ac1b4/t/616ff36761fabc72642806e3/1634726781251/TPC_FullColor_TransparentBg_1280x1024_72dpi.png' })
-        .setTimestamp()
-      if (data.s1 !== 0) {
-        embed.addFields({ name: 'S1 Hours:', value: `${data.s1}` })
-      }
-      if (data.s2 !== 0) {
-        embed.addFields({ name: 'S2 Hours:', value: `${data.s2}` })
-      }
-      if (data.s3 !== 0) {
-        embed.addFields({ name: 'S3 Hours:', value: `${data.s3}` })
-      }
-      if (data.c1 !== 0) {
-        embed.addFields({ name: 'C1 Hours:', value: `${data.c1}` })
-      }
-      if (data.c3 !== 0) {
-        embed.addFields({ name: 'C3 Hours:', value: `${data.c3}` })
-      }
-      if (data.i1 !== 0) {
-        embed.addFields({ name: 'I1 Hours:', value: `${data.i1}` })
-      }
-      if (data.i3 !== 0) {
-        embed.addFields({ name: 'I3 Hours:', value: `${data.i3}` })
-      }
-      if (data.sup !== 0) {
-        embed.addFields({ name: 'Supervisor Hours:', value: `${data.sup}` })
-      }
-      if (data.adm !== 0) {
-        embed.addFields({ name: 'Administrator Hours:', value: `${data.adm}` })
-      }
-      await interaction.reply({ embeds: [embed], ephemeral: true })
+        .setTimestamp();
+
+        const labels = {
+          s1: 'S1 Hours',
+          s2: 'S2 Hours',
+          s3: 'S3 Hours',
+          c1: 'C1 Hours',
+          c3: 'C3 Hours',
+          i1: 'I1 Hours',
+          i3: 'I3 Hours',
+          sup: 'Supervisor Hours',
+          adm: 'Administrator Hours',
+        };
+        
+        for (const key in hoursData) {
+          if (hoursData[key] !== 0) {
+            embed.addFields({ name: labels[key], value: `${hoursData[key]}` });
+          }
+        }
+        
+      await interaction.reply({ embeds: [embed], ephemeral: true });
     }
-  }
-}
+  },
+};
