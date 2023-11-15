d82acfa7c9db29a23081ea346b237291234ead36
michaelcmelton <michaelcmelton93@gmail.com>
2023-11-15 08:16:07 -0500

    fix(utils): Update export syntax for commonJS

--- utils.js
+++ utils.js
@@ -1,9 +1,11 @@
 const Sentry = require("@sentry/node")
 
-export function sendToSentry(err, locationLabel) {
+function sendToSentry(err, locationLabel) {
     Sentry.withScope(function (scope) {
         // group errors together based on their request and response
         scope.setFingerprint([locationLabel, err.toString()]);
         Sentry.captureException(err);
     });
 }
+
+module.exports = {sendToSentry}
