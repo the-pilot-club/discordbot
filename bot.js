e498e26402d8172e46fad3cf01b982fe3987948f
loshisthebest <lucasimmerman2@gmail.com>
2023-11-27 20:54:21 -0500

    added hidden answers to quiz

--- bot.js
+++ bot.js
@@ -167,13 +167,13 @@ async function sendNewAnswer (channel) {
   const answerBody = await answer.json()
   switch (answerBody.correctAnswer) {
     case 'A':
-      channel.send(`<:training_team:895480894901592074> The correct answer is 🇦 ${answerBody.optionA}`)
+      channel.send(`<:training_team:895480894901592074> The correct answer is ||🇦 ${answerBody.optionA}||`)
       break
     case 'B':
-      channel.send(`<:training_team:895480894901592074> The correct answer is 🇧 ${answerBody.optionB}`)
+      channel.send(`<:training_team:895480894901592074> The correct answer is ||🇧 ${answerBody.optionB}||`)
       break
     case 'C':
-      channel.send(`<:training_team:895480894901592074> The correct answer is 🇨 ${answerBody.optionC}`)
+      channel.send(`<:training_team:895480894901592074> The correct answer is ||🇨 ${answerBody.optionC}||`)
       break
   }
 }
