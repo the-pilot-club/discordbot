39579acee8339e98f0f5495b7a5e29b097fef5fb
michaelcmelton <michaelcmelton93@gmail.com>
2024-01-23 21:27:05 -0500

    feat(screenshot): Add logging for further debugging when it happens again

--- events/tpcreaction.js
+++ events/tpcreaction.js
@@ -2,11 +2,15 @@ module.exports = {
     name: 'messageCreate',
     once: false,
     execute (message) {
-      if (message.channel.id === '830210202464813056' || (message.channel.name.startsWith('SCREENSHOT CONTEST') && message.channel.parentId === '830210202464813056')) {
-        const image = message.attachments.find(attachment => attachment.contentType?.startsWith('image/'));
-        if (image) {
-          message.react(':tpc:845075689241051138').catch(err => console.error(err));
+      try {
+        if (message.channel.id === '830210202464813056' || (message.channel.name.startsWith('SCREENSHOT CONTEST') && message.channel.parentId === '830210202464813056')) {
+          const image = message.attachments.find(attachment => attachment.contentType?.startsWith('image/'));
+          if (image) {
+            message.react(':tpc:845075689241051138').catch(err => console.error(err));
+          }
         }
-      }
+      } catch (error) {
+         console.error(JSON.parse(`{Channel: ${JSON.stringify(message.channel)}, User: ${JSON.stringify(message.user)}, Error: ${error}, Time: ${Date.now()}}`));
+        }
     }
   }
