076736636ee675f8642994a7cbda2a480b954bdf
michaelcmelton <michaelcmelton93@gmail.com>
2024-01-23 21:32:17 -0500

    feat(bot): revert event removal

--- bot.js
+++ bot.js
@@ -107,46 +107,46 @@ client.on('guildMemberUpdate', async (oldMember, newMember) => {
 const cron = require('node-cron')
 const {sendToSentry} = require("./utils");
 
-// async function sendNewEvent(channel, pings) {
-//   try {
-//     const guild = channel.client.guilds.cache.find(guild => {
-//       return guild.id === process.env.GUILD_ID
-//     })
-//
-//     if (guild === undefined) {
-//       throw new Error(`guild not found in cache for guild id "${process.env.GUILD_ID}"`)
-//     }
-//
-//     const events = await guild.scheduledEvents.fetch()
-//
-//     if (events.size === 0) {
-//       throw new Error(`failed to fetch scheduled events for guild id "${process.env.GUILD_ID}"`)
-//     }
-//
-//     const sortedEvents = events.sort((a, b) => a.scheduledStartAt - b.scheduledStartAt)
-//     const nextEvent = sortedEvents.first()
-//     const now = new Date()
-//     if (Math.abs(nextEvent.scheduledStartAt - now) <= 60 * 60 * 1000 && !channel.client.eventReminders.includes(nextEvent.id)) {
-//       const day = nextEvent.scheduledStartAt.getDay()
-//       const image = new AttachmentBuilder( nextEvent.coverImageURL({size: 4096, extension: "jpeg"}), 'event-banner.jpeg')
-//       if (nextEvent.image !== null) {
-//         channel.send({
-//           content: pings[day] + "\n **The event is starting in 1 hour. See you there!**\n" + nextEvent.description + "\n",
-//           files: [image]
-//         })
-//       } else {
-//         channel.send({
-//           content: pings[day] + "\n" + "**The event is starting in 1 hour.**\n" + nextEvent.description,
-//         })
-//       }
-//
-//       channel.client.eventReminders.push(nextEvent.id)
-//     }
-//   } catch (error) {
-//     console.log(error)
-//     sendToSentry(error, 'event-ping')
-//   }
-// }
+async function sendNewEvent(channel, pings) {
+  try {
+    const guild = channel.client.guilds.cache.find(guild => {
+      return guild.id === process.env.GUILD_ID
+    })
+
+    if (guild === undefined) {
+      throw new Error(`guild not found in cache for guild id "${process.env.GUILD_ID}"`)
+    }
+
+    const events = await guild.scheduledEvents.fetch()
+
+    if (events.size === 0) {
+      throw new Error(`failed to fetch scheduled events for guild id "${process.env.GUILD_ID}"`)
+    }
+
+    const sortedEvents = events.sort((a, b) => a.scheduledStartAt - b.scheduledStartAt)
+    const nextEvent = sortedEvents.first()
+    const now = new Date()
+    if (Math.abs(nextEvent.scheduledStartAt - now) <= 60 * 60 * 1000 && !channel.client.eventReminders.includes(nextEvent.id)) {
+      const day = nextEvent.scheduledStartAt.getDay()
+      const image = new AttachmentBuilder( nextEvent.coverImageURL({size: 4096, extension: "jpeg"}), 'event-banner.jpeg')
+      if (nextEvent.image !== null) {
+        channel.send({
+          content: pings[day] + "\n **The event is starting in 1 hour. See you there!**\n" + nextEvent.description + "\n",
+          files: [image]
+        })
+      } else {
+        channel.send({
+          content: pings[day] + "\n" + "**The event is starting in 1 hour.**\n" + nextEvent.description,
+        })
+      }
+
+      channel.client.eventReminders.push(nextEvent.id)
+    }
+  } catch (error) {
+    console.log(error)
+    sendToSentry(error, 'event-ping')
+  }
+}
 
 
 async function sendNewQuestion (channel) {
@@ -211,9 +211,9 @@ client.on('ready', async function () {
     sendNewQuestion(channel)
   })
 
-  // cron.schedule('* * * * *', function () {
-  //   sendNewEvent(eventChannel, weeklyPings)
-  // })
+  cron.schedule('* * * * *', function () {
+    sendNewEvent(eventChannel, weeklyPings)
+  })
 })
 
 module.exports = client
