6398f15a17312ce82cd55e463bcc885ad5cb2b15
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2024-02-29 21:31:37 -0800

    fix the incorrect \\n on both events

--- cron-jobs/functions/sendNewEvent.js
+++ cron-jobs/functions/sendNewEvent.js
@@ -30,12 +30,12 @@ export async function sendNewEvent(channel, pings) {
             }), 'event-banner.jpeg')
             if (nextEvent.image !== null) {
                 channel.send({
-                    content: pings[day] + "\n **The event is starting in 1 hour. See you there!**\n" + nextEvent.description + `\\n\\n Hosted by ${nextEvent.creator} \\n`,
+                    content: pings[day] + "\n **The event is starting in 1 hour. See you there!**\n" + nextEvent.description + `\n\n Hosted by ${nextEvent.creator} \n`,
                     files: [image]
                 })
             } else {
                 channel.send({
-                    content: pings[day] + "\n" + "**The event is starting in 1 hour.**\n" + nextEvent.description + `\\n\\n Hosted by ${nextEvent.creator} \\n`,
+                    content: pings[day] + "\n" + "**The event is starting in 1 hour.**\n" + nextEvent.description + `\n\n Hosted by ${nextEvent.creator} \n`,
                 })
             }
 
