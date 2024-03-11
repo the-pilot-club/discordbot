baaa34b7a8b3880496c7eacd236e810b7cf0fd29
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2024-03-10 22:54:24 -0700

    DST fix

--- cron-jobs/index.js
+++ cron-jobs/index.js
@@ -16,15 +16,15 @@ export async function cronJobs(client){
     }
 // Getting random question every day: 0 57 22 * * *
 // Sends Answer to current Question
-    cron.schedule('0 52 13 * * *', function () { // Correct time is 0 52 13 * * *
+    cron.schedule('0 52 12 * * *', function () { // Correct time is 0 52 13 * * *
         sendNewAnswer(channel)
     })
 // Sends an API Call to change the current question
-    cron.schedule('0 58 13 * * *', function () { // Correct time is 0 58 13 * * *
+    cron.schedule('0 58 12 * * *', function () { // Correct time is 0 58 13 * * *
         updateQuestion()
     })
 // Sends the new question.
-    cron.schedule('0 00 14 * * *', function () { // Correct time is 0 00 14 * * *
+    cron.schedule('0 00 13 * * *', function () { // Correct time is 0 00 14 * * *
         sendNewQuestion(channel)
     })
 
