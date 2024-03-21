866e63e28b4e352dc850fcfe78f1cf40c3cfbe72
jonahkleinman <97543307+jonahkleinman@users.noreply.github.com>
2024-03-21 20:34:00 +0000

    Add addlog for eric to look at

---
+++ commands/fcp/addlog.js
@@ -0,0 +1,78 @@
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import fetch from 'node-fetch';
+import { Config } from '../../config/config.js';
+import { sendToSentry } from '../../utils.js';
+
+const config = new Config();
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('add-log')
+    .setDescription('Add a FCP audit log to a member')
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('The user to add the log to')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName('log-reason')
+        .setDescription('Log content')
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const user = interaction.options.getUser("user");
+    const logReason = interaction.options.getString("log-reason");
+    
+    const response = await fetch(`${config.fcpBaseUrl()}/api/users/find/${user.id}/audit-logs/new`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json', // Set Accept header to application/json
+            'Authorization': `Bearer TOKEN`
+            // TODO: Add FCP_TOKEN as a env variable
+        },
+        body: JSON.stringify({
+            submitter: interaction.user.id,
+            log: logReason,
+        }),
+    });
+
+
+    if (!response || response.status !== 202) {
+      console.log(response.status)
+      console.log(
+        `The Response could not be completed as dialed for ${interaction.member.displayName}`,
+      );
+      interaction.reply({
+        content:
+          "This Function could not be completed as dialed. Please try again later",
+        ephemeral: true,
+      });
+      return;
+    }
+
+    const body = await response.json();
+    const notFound = body.detail;
+
+    if (notFound === "Not Found") {
+     // TODO: Create a FCP account if user doesn't exist
+    } else {
+      const successEmbed = new EmbedBuilder()
+        .setAuthor({
+          name: `${interaction.member.displayName}`,
+          iconURL: `${interaction.user.displayAvatarURL()}`,
+        })
+        .setDescription(`Successfully created a new audit log for ${user}.`)
+        .setColor("#22bb33")
+        .setFooter({
+          text: "Made by TPC Dev Team",
+          iconURL:
+            "https://static1.squarespace.com/static/614689d3918044012d2ac1b4/t/616ff36761fabc72642806e3/1634726781251/TPC_FullColor_TransparentBg_1280x1024_72dpi.png",
+        })
+        .setTimestamp();
+      await interaction.reply({ embeds: [successEmbed] });
+    }
+  },
+};
