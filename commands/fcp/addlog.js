c4e4d6cb354d5289b26202fcbd8c65251c75b76c
jonahkleinman <97543307+jonahkleinman@users.noreply.github.com>
2024-03-23 11:18:16 +0100

    Error handling

--- commands/fcp/addlog.js
+++ commands/fcp/addlog.js
@@ -22,56 +22,50 @@ export default {
         .setRequired(true)
     ),
   async execute(interaction) {
-    const user = interaction.options.getUser("user");
-    const logReason = interaction.options.getString("log-reason");
-    
-    const response = await fetch(`${config.fcpBaseUrl()}/api/users/find/${user.id}/audit-logs/new`, {
+    try {
+      const user = interaction.options.getUser("user");
+      const logReason = interaction.options.getString("log-reason");
+
+      const response = await fetch(`https://flightcrew-beta.thepilotcub.org/api/users/find/${user.id}/audit-logs/new`, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization': `Bearer ${process.env.FCP_TOKEN}`
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${process.env.FCP_TOKEN}`
         },
         body: JSON.stringify({
-            staffId: interaction.user.id,
-            log: logReason,
+          staffId: interaction.user.id,
+          log: logReason,
         }),
-    });
+      });
 
-
-    if (!response || response.status !== 202) {
-      console.log(response.status)
-      console.log(
-        `The Response could not be completed as dialed for ${interaction.member.displayName}`,
-      );
+      if (response.status === 404) {
+        // TODO: Create a FCP account if user doesn't exist
+      } else if (!response.ok) {
+        throw new Error(`HTTP error. Status: ${response.status}`);
+      } else {
+        const body = await response.json();
+        const successEmbed = new EmbedBuilder()
+          .setAuthor({
+            name: `${interaction.member.displayName}`,
+            iconURL: `${interaction.user.displayAvatarURL()}`,
+          })
+          .setDescription(`Successfully created a new audit log for ${user}.`)
+          .setColor("#22bb33")
+          .setFooter({
+            text: "Made by TPC Dev Team",
+            iconURL: "https://static1.squarespace.com/static/614689d3918044012d2ac1b4/t/616ff36761fabc72642806e3/1634726781251/TPC_FullColor_TransparentBg_1280x1024_72dpi.png",
+          })
+          .setTimestamp();
+        await interaction.reply({ embeds: [successEmbed] });
+      }
+    } catch (error) {
+      console.error('Failed to add log:', error);
       interaction.reply({
-        content:
-          "This Function could not be completed as dialed. Please try again later",
+        content: "There was an error while attempting to add the log. Please try again later.",
         ephemeral: true,
       });
-      return;
-    }
-
-    const body = await response.json();
-    const notFound = body.detail;
-
-    if (notFound === "Not Found") {
-     // TODO: Create a FCP account if user doesn't exist
-    } else {
-      const successEmbed = new EmbedBuilder()
-        .setAuthor({
-          name: `${interaction.member.displayName}`,
-          iconURL: `${interaction.user.displayAvatarURL()}`,
-        })
-        .setDescription(`Successfully created a new audit log for ${user}.`)
-        .setColor("#22bb33")
-        .setFooter({
-          text: "Made by TPC Dev Team",
-          iconURL:
-            "https://static1.squarespace.com/static/614689d3918044012d2ac1b4/t/616ff36761fabc72642806e3/1634726781251/TPC_FullColor_TransparentBg_1280x1024_72dpi.png",
-        })
-        .setTimestamp();
-      await interaction.reply({ embeds: [successEmbed] });
+      sendToSentry(error, "Audit log command");
     }
   },
 };
