4ce1b798fe80db25520cae2156d1b73acb5c1822
jonahkleinman <97543307+jonahkleinman@users.noreply.github.com>
2024-03-29 23:07:30 -0400

    Migrate quiz to new api

--- cron-jobs/functions/sendNewAnswer.js
+++ cron-jobs/functions/sendNewAnswer.js
@@ -1,18 +1,23 @@
-import {Config} from "../../config/config.js";
-const config = new Config()
+import { Config } from "../../config/config.js";
+const config = new Config();
 export async function sendNewAnswer(channel) {
-    const apiUrl = config.questionsApiUrl()
-    const answer = await fetch(apiUrl)
-    const answerBody = await answer.json()
-    switch (answerBody.correctAnswer) {
-        case 'A':
-            channel.send(`<:training_team:895480894901592074> The correct answer is ||🇦 ${answerBody.optionA}||`)
-            break
-        case 'B':
-            channel.send(`<:training_team:895480894901592074> The correct answer is ||🇧 ${answerBody.optionB}||`)
-            break
-        case 'C':
-            channel.send(`<:training_team:895480894901592074> The correct answer is ||🇨 ${answerBody.optionC}||`)
-            break
-    }
+  const answer = await fetch(`${config.quizBaseUrl()}/api/quiz/current`)
+  const answerBody = await answer.json();
+  switch (answerBody.correctAnswer) {
+    case "A":
+      channel.send(
+        `<:training_team:895480894901592074> The correct answer is ||🇦 ${answerBody.optionA}||`,
+      );
+      break;
+    case "B":
+      channel.send(
+        `<:training_team:895480894901592074> The correct answer is ||🇧 ${answerBody.optionB}||`,
+      );
+      break;
+    case "C":
+      channel.send(
+        `<:training_team:895480894901592074> The correct answer is ||🇨 ${answerBody.optionC}||`,
+      );
+      break;
+  }
 }
