8bee4cd40c3d49f09b9eca1d126d6878c08509bd
jonahkleinman <97543307+jonahkleinman@users.noreply.github.com>
2024-09-20 20:12:46 -0400

    288 - Submits automod infractions to FCP and fixes addlog command

--- events/automod.js
+++ events/automod.js
@@ -1,6 +1,9 @@
 import {
     ChannelType,
 } from 'discord.js';
+import {Config} from "../config/config.js";
+const config = Config
+import {sendToSentry} from "../utils.js";
 
 export async function autoMod(action) {
     if (action.action.type === 2 || action.action.type === 3) {
@@ -20,6 +23,22 @@ export async function autoMod(action) {
     const oldThread = threadChannel.threads.cache.find(
         (channel) => channel.name === `Private - ${action.member.displayName}`,
     );
+
+    let logMessage = `Member triggered auto moderation within the discord server. Reason: `;
+    if (action.matchedKeyword !== null && action.matchedKeyword !== 'game') {
+        logMessage += `Sent blocked word "${action.matchedKeyword}".`;
+    } else if (action.ruleTriggerType === 3) {
+        logMessage += 'Message flagged as spam.';
+    } else if (action.ruleTriggerType === 4) {
+        logMessage += 'Used a word from Discord\'s predefined blocked words list.';
+    } else if (action.ruleTriggerType === 5) {
+        logMessage += 'Attempted to mention multiple roles.';
+    } else if (action.ruleTriggerType === 6) {
+        logMessage += 'Profile flagged as an issue.';
+    } else {
+        logMessage += 'Unknown reason.';
+    }
+
     if (oldThread) {
         let message = `${action.user.toString()} You have sent something in The Pilot Club that is a part of our auto moderation. Here is the reason for the block:\n\n`;
         if (action.matchedKeyword !== null && action.matchedKeyword !== 'game') {
@@ -83,5 +102,28 @@ export async function autoMod(action) {
                 thread.send(message);
                 thread.setLocked(true);
             });
+    }
+
+    if (action.matchedKeyword === 'game') {
+        return;
+    }
+
+    const url = `${config.fcpBaseUrl()}/api/users/find/${action.user.id}/audit-logs/new`;
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${process.env.FCP_TOKEN}`,
+        },
+        body: JSON.stringify({
+            'user_id': action.user.id,
+            'submitted_by': '968312697403871232',
+            'text': logMessage,
+        }),
+    });
+    if (!response.ok) {
+        const errorDetails = await response.text();
+        sendToSentry(errorDetails, "Automod Audit Log")
     }
 }
