b4f43e7ed8c03273a90d7eb7f0bcb69c238faa0d
vFlyingCowboy <wethepeoplesteiner@gmail.com>
2024-10-28 16:18:39 -0700

    turn off message responses and update perks giveaway command

--- commands/giveaway/perks-giveaway.js
+++ commands/giveaway/perks-giveaway.js
@@ -6,11 +6,33 @@ export default {
     .setDescription('Picks random user with company perks role(s)'),
   async execute (interaction) {
     if (interaction.member.roles.cache.some(role => role.name === 'Staff') || interaction.member.roles.cache.some(role => role.name === 'Air Marshals')) {
+      interaction.deferReply()
       interaction.guild.members.fetch().then(members => {
-        const result = members.filter(m => m.roles.cache.find(role => role.name === 'VIP' || role.name === 'Commuter' ||
-                    role.name === 'Frequent Flyer' || role.name === 'Early Adopters' || role.name === 'Charters Top Gun'))
-        const tags = result.map(m => m.user.toString())
-        const winner = tags[Math.floor(Math.random() * tags.length)]
+        const entries = []
+        const vip = members.filter(m => m.roles.cache.find(role => role.name === 'VIP')).map(m => m.user.toString())
+        for (const vipEntry of vip){
+          entries.push(vipEntry)
+          entries.push(vipEntry)
+          entries.push(vipEntry)
+        }
+        const frequentFliers = members.filter(m => m.roles.cache.find(role => role.name === 'Frequent Flyer')).map(m => m.user.toString())
+        for (const ffEntry of frequentFliers){
+          entries.push(ffEntry)
+          entries.push(ffEntry)
+        }
+        const commuters = members.filter(m => m.roles.cache.find(role => role.name === 'Commuter')).map(m => m.user.toString())
+        for (const entry of commuters){
+          entries.push(entry)
+        }
+        const ea = members.filter(m => m.roles.cache.find(role => role.name === 'Early Adopters')).map(m => m.user.toString())
+        for (const entry of ea){
+          entries.push(entry)
+        }
+        const topGun = members.filter(m => m.roles.cache.find(role => role.name === 'Charters Top Gun')).map(m => m.user.toString())
+        for (const entry of topGun){
+          entries.push(entry)
+        }
+        const winner = entries[Math.floor(Math.random() * entries.length)]
         if (winner !== undefined) {
           const winnere = new EmbedBuilder()
             .setDescription(`And the winner is ${winner} Congratulations!`)
@@ -22,7 +44,7 @@ export default {
             .setFooter({ text: 'Made by TPC Dev Team' })
             .setTimestamp()
 
-          interaction.deferReply()
+
           setTimeout(function () {
             interaction.editReply({ content: `Congrats ${winner}!`, embeds: [winnere] })
           }, 3000)
