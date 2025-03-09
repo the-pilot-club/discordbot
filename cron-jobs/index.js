a910d5dddb8e14d315d4fc9810b0c3824df80714
vFlyingCowboy <wethepeoplesteiner@gmail.com>
2025-03-08 23:20:21 -0800

    update times for DST

--- cron-jobs/index.js
+++ cron-jobs/index.js
@@ -1,8 +1,8 @@
 import cron from "node-cron";
-import {sendNewAnswer} from "./functions/sendNewAnswer.js";
-import {sendNewQuestion} from "./functions/sendNewQuestion.js";
-import {sendNewEvent} from "./functions/sendNewEvent.js";
-export async function cronJobs(client){
+import { sendNewAnswer } from "./functions/sendNewAnswer.js";
+import { sendNewQuestion } from "./functions/sendNewQuestion.js";
+import { sendNewEvent } from "./functions/sendNewEvent.js";
+export async function cronJobs(client) {
     const channel = await client.channels.cache.find(channel => channel.name === 'aviation-quiz')
     const eventChannel = await client.channels.cache.find(channel => channel.name === 'crew-chat')
     const weeklyPings = {
@@ -13,17 +13,17 @@ export async function cronJobs(client){
         5: '<@&937389346204557342>', //Friday
         6: '<@&937389346204557342>'  //Saturday
     }
-// Getting random question every day: 0 57 22 * * *
-// Sends Answer to current Question
-    cron.schedule('0 52 13 * * *', function () { // Correct time is 0 52 13 * * *
+    // Getting random question every day: 0 57 22 * * *
+    // Sends Answer to current Question
+    cron.schedule('0 52 12 * * *', function() { // Correct time is 0 52 13 * * *
         sendNewAnswer(channel)
     })
-// Sends the new question.
-    cron.schedule('0 00 14 * * *', function () { // Correct time is 0 00 14 * * *
+    // Sends the new question.
+    cron.schedule('0 00 13 * * *', function() { // Correct time is 0 00 14 * * *
         sendNewQuestion(channel)
     })
 
-    cron.schedule('* * * * *', function () {
+    cron.schedule('* * * * *', function() {
         sendNewEvent(eventChannel, weeklyPings)
     })
 }
